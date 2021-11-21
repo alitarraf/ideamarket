363d5844a84992d89ec7981dc723c282534e0720
Ali <ali.tarraf@gmail.com>
2021-11-20 20:54:45 -0800

    front end idea page

--- packages/react-app/src/components/idea/index.js
+++ packages/react-app/src/components/idea/index.js
@@ -1,3 +1,4 @@
 export { default as IdeaDescriptionCard } from "./IdeaDescriptionCard";
 export { default as PeopleCard } from "./PeopleCard";
 export { default as MilestonesCard } from "./MilestonesCard";
+export { default as TreasuryCard } from "./TreasuryCard";
