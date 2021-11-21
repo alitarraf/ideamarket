971febeeca17282d6a44038e04fc69e801f1adb9
e-roy <ericroupe@gmail.com>
2021-11-21 06:49:04 -0500

    part 2

--- packages/react-app/src/components/idea/FundingCard.jsx
+++ packages/react-app/src/components/idea/FundingCard.jsx
@@ -33,7 +33,7 @@ const FundingCard = () => {
                     single contribution
                   </span>
                 </div>
-                <div className="text-center mt-8 mb-8 mt-auto">
+                <div className="text-center mt-8 mb-8">
                   <div className="inline-block ml-3">
                     <button className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-blue-600 rounded shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none">
                       Support
